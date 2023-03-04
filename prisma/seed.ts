de3fbf669de1e134d7a6af917599e83521e8b546
ConstantinoRafael <rafael.constantino89@gmail.com>
2023-03-04 17:18:05 -0300

    feat: seed ticket types

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import dayjs from "dayjs";
+import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
 const prisma = new PrismaClient();
 
 async function main() {
@@ -7,16 +7,65 @@ async function main() {
   if (!event) {
     event = await prisma.event.create({
       data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+        title: 'Driven.t',
+        logoImageUrl: 'https://files.driveneducation.com.br/images/logo-rounded.png',
+        backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
         startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
+        endsAt: dayjs().add(21, 'days').toDate(),
       },
     });
   }
 
-  console.log({ event });
+  let ticketOnline = await prisma.ticketType.findFirst({
+    where: {
+      isRemote: true,
+    },
+  });
+  if (!ticketOnline) {
+    ticketOnline = await prisma.ticketType.create({
+      data: {
+        name: 'online',
+        price: 100,
+        isRemote: true,
+        includesHotel: false,
+      },
+    });
+  }
+
+  let ticketWithHotel = await prisma.ticketType.findFirst({
+    where: {
+      includesHotel: true,
+    },
+  });
+  if (!ticketWithHotel) {
+    ticketWithHotel = await prisma.ticketType.create({
+      data: {
+        name: 'presencial',
+        price: 300,
+        isRemote: false,
+        includesHotel: true,
+      },
+    });
+  }
+
+  let ticketWithoutHotel = await prisma.ticketType.findFirst({
+    where: {
+      isRemote: false,
+      includesHotel: false,
+    },
+  });
+  if (!ticketWithoutHotel) {
+    ticketWithoutHotel = await prisma.ticketType.create({
+      data: {
+        name: 'presencial',
+        price: 300,
+        isRemote: false,
+        includesHotel: false,
+      },
+    });
+  }
+
+  console.log({ event, ticketOnline, ticketWithHotel, ticketWithoutHotel });
 }
 
 main()
